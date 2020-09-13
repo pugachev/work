6d3d46bd2f17f8672b3c68d4f7e9e594210f763e
池フクロウ <pugachev2011@gmail.com>
2020-09-14 07:39:24 +0900

    商品一覧をだすところまで作りました。

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -9,9 +9,50 @@ use DateTime;
 
 class WorkController extends Controller
 {
-    public function list() 
-    {
-        $records = Shohin::all();
-        return view('savemoney.list')->with('records', $records);
-    }
+        public function list() 
+        {
+            $records = Shohin::all();
+            return view('work.list')->with('records', $records);
+        }
+        
+        public function create() 
+        {
+            return view('work.input');
+        }
+
+        public function store(Request $request) 
+        {
+        $record = new Shohin();
+        $record->money = $request->inputMoney;
+        $rcvdate = DateTime::createFromFormat('m/d/Y',$request->inputDate);
+        $record->targetDate = $rcvdate->format("Y-m-d");
+        $record->result='○';
+        $record->save();
+        return redirect('/verification');
+        }
+
+      public function edit(Record $record) {
+        $selectRecord = Record::find($record->id);
+        $rcvdate = DateTime::createFromFormat('Y-m-d', $selectRecord->targetDate)->format('m/d/Y');
+        $selectRecord->targetDate = $rcvdate;
+        return view('savemoney.edit')->with('selectRecord', $selectRecord);
+      }
+
+      public function update(Request $request,Record $record) {
+        $record->money = $request->inputMoney;
+        $rcvdate = DateTime::createFromFormat('m/d/Y',$request->inputDate);
+        $record->targetDate = $rcvdate->format("Y-m-d");
+        $originalImg = $request->inputFile;
+        // dd($originalImg);
+        $filePath = $originalImg->store('public');
+        $record->imgpath = str_replace('public/', '', $filePath);
+        $record->result='○';
+        $record->save();
+        return redirect('/verification');
+      }
+
+      public function destroy(Request $request,Record $record) {
+        $record->delete();
+        return redirect('/verification');
+      }
 }
---
+++ resources/views/work/list.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <title>商品一覧</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <style>
+        .notification-badge {
+        position:relative;
+        }
+        .notification-badge[data-badge]:after {
+        content:attr(data-badge);
+        position:absolute;
+        top:-8x;
+        right:-10px;
+        font-size:.8em;
+        background:green;
+        color:white;
+        width:18px;height:18px;
+        text-align:center;
+        line-height:18px;
+        border-radius:50%;
+        box-shadow:0 0 1px #333;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-sm navbar-light">
+                <a href="" class="navbar-brand">商品一覧</a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#menu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div id="menu" class="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="{{ url('/create') }}" class="nav-link">商品登録</a></li>
+                        <li class="nav-item"><a href="{{ url('/create') }}" class="nav-link">ログアウト</a></li>
+                        <li class="nav-item"><a href="{{ url('/create') }}" data-badge="27" class="notification-badge nav-link">カート</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section class="bg-light text-center py-5">
+            <h2 class="mb-5">商品一覧</h2>
+            <div class="container">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>商品名</th>
+                            <th>カテゴリ</th>
+                            <th>販売価格</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($records as $record)
+                            <tr>
+                                <td>{{$record->shohin_id}}</td>
+                                <td>{{$record->shohin_name}}</td>
+                                <td>{{$record->category}}</td>
+                                <td>{{$record->hanbai_tanka}}</td>
+
+                            </tr>
+                        @empty
+                        
+                        @endforelse
+
+                    </tbody>
+                </table>
+            </div>
+        </section>
+
+    </main>
+    <footer class="fixed-bottom text-center text-muted py-4">
+        Copyright 2020-09 mtake.com
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <script src="{{ asset('/js/main.js') }}"></script>
+</body>
+
+</html>
