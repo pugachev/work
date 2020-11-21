3dc65e468a13f744df54b67c6d4c88a4f7e16ee8
pugachev <pugachev2011@gmail.com>
2020-11-21 18:56:03 +0900

    mixhostで作成したものです

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,10 +16,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home', 'WorkController@list');
-Route::get('logout', 'WorkController@logout');
-Route::post('addcart', 'WorkController@addcart');
